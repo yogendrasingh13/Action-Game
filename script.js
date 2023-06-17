0572ce3f80915cbce4d41b0d401f78fd38b5d31b
yogendrasingh13 <yogendrasingh1330@gmail.com>
2023-06-17 07:27:51 +0530

    modified
    music play and pause

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+Score = 0;
+cross = true;
+
+audio = new Audio('music.mp3');
+audiogo = new Audio('gameover.mp3');
+   setTimeout(() => {
+    audio.play()
+   }, 1000);
 document.onkeydown = function (e) {
     if(e.keyCode == 38) {
         dino = document.querySelector('.dino');
@@ -5,6 +13,16 @@ document.onkeydown = function (e) {
         setTimeout(() => {
             dino.classList.remove('animateDino')
         }, 700);
+    }
+    if(e.keyCode==39) {
+        dino= document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+    if(e.keyCode==37) {
+        dino= document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112) + "px";
     }
 }
 setInterval(() => {
@@ -21,8 +39,32 @@ setInterval(() => {
     offsetX = Math.abs(dx-ox);
     offsetY = Math.abs(dy-oy);
 
-    if(offsetX<93 && offsetY<52) {
-        gameOver.style.visibility = 'visible';
+    if(offsetX<113 && offsetY<52) {
+        gameOver.innerHTML = "Game Over"
         obstacle.classList.remove('obstacleAni');
+        audio.pause();
+        audiogo.play();
+        setTimeout(() => {
+            audiogo.pause();
+            
+        }, 2500);
     }
-}, 100);
+    else if(offsetX<145 && cross){
+        Score += 1;
+        updateScore(Score);
+        cross = false;
+        setTimeout(() => {
+            cross = true;
+        }, 1000);
+        setTimeout(() => {
+            aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.1;
+            obstacle.style.animationDuration = newDur + 's'; 
+        }, 5000);
+        
+    }
+}, 10);
+
+function updateScore(score) {
+    scoreCount.innerHTML = "Your Score is: " + score
+}
